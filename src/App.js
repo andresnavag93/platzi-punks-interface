afadd734ad9e76b7472a60875de02649d18cdac8
Andres Navarro <andresnavag93@gmail.com>
2021-12-09 12:34:25 -0400

    :hammer: Basic Layout with Wallet Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route } from "react-router-dom";
 import Home from "./views/home";
+import MainLayout from "./layouts/main";
 
 function App() {
   return (
-    <>
+    <MainLayout>
       <Route path="/" exact component={Home} />
-    </>
+    </MainLayout>
   );
 }
 
